e22916638487b5826f801dc5dd8f347ce8276e0c
RubenHaisma <74268046+RubenHaisma@users.noreply.github.com>
2025-07-22 09:33:14 +0200

    gtm

--- app/layout.tsx
+++ app/layout.tsx
@@ -67,8 +67,18 @@ export default function RootLayout({
             }}
           />
         )}
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" dangerouslySetInnerHTML={{
+          __html: `\n(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':\nnew Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],\nj=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=\n'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);\n})(window,document,'script','dataLayer','GTM-WXD3JZPR');\n`
+        }} />
+        {/* End Google Tag Manager */}
       </head>
       <body className="antialiased">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WXD3JZPR" height="0" width="0" style={{display:'none',visibility:'hidden'}}></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         {children}
       </body>
     </html>
